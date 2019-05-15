df618f49bbff364612e72d252d4574aa5767f7d7
ydd-mac <1019132983@qq.com>
2019-05-15 10:54:37 +0800

    一些小控件加入

--- lib/pages/personal_page.dart
+++ lib/pages/personal_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shop/weight/src/cached_image_widget.dart';
 
 import '../weight/flutter_screenutil.dart';
 
@@ -11,10 +12,11 @@ class PersonalPage extends StatelessWidget {
       ),
       body: ListView(
         children: <Widget>[
+          /* _demoWidget(context),*/
           _topHeader(),
           _orderTitle(),
           _orderType(),
-          _actionList()
+          _actionList(),
         ],
       ),
     );
@@ -32,6 +34,9 @@ class PersonalPage extends StatelessWidget {
             child: ClipOval(
               child: Image.network(
                 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1556532393846&di=2817eb4509ed9d604fbd59abc4259013&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F00bc8151242c7d2460d0b7d4b913c6ed97f957cc158f9-SXd0Yk_fw658',
+                width: 200.0,
+                height: 200.0,
+                fit: BoxFit.cover,
               ),
             ),
           ),
@@ -155,5 +160,96 @@ class PersonalPage extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  Widget _demoWidget(context) {
+    return Column(
+      children: <Widget>[
+        CachedNetworkImage(
+          //带缓存的网络图片
+          imageUrl:
+              'http://pic15.nipic.com/20110628/1369025_192645024000_2.jpg',
+          placeholder: (context, url) => new CircularProgressIndicator(),
+          errorWidget: (context, url, error) => new Icon(Icons.error),
+        ),
+        CloseButton(),
+        BackButton(),
+        FractionallySizedBox(
+            //盛满宽度
+            widthFactor: 1,
+            child: Container(
+              decoration: BoxDecoration(color: Colors.red),
+              child: InkWell(
+                child: Text('11111111'),
+                onTap: () {
+                  _bottomDialog(context);
+                },
+              ),
+            )),
+        Image(width: 100, height: 100, image: AssetImage('images/aaa.png')),
+        Chip(
+          label: Text('个人中心chip'),
+          /*avatar: CircleAvatar(
+            backgroundColor: Colors.blue,
+            child: Text('1'),
+          ),*/
+          avatar: Icon(
+            Icons.people,
+            color: Colors.blue,
+            size: 20,
+          ),
+          backgroundColor: Colors.red,
+        ),
+        ClipRRect(
+          //圆角矩形
+          borderRadius: BorderRadius.circular(50),
+          child: Image(
+              image: AssetImage('images/aaa.png'), width: 100, height: 100),
+        ),
+        ClipOval(
+          //圆形裁剪
+          child: Image.asset(
+            "images/aaa.png",
+            width: 100.0,
+            height: 100.0,
+            fit: BoxFit.cover,
+          ),
+        ),
+        CircleAvatar(
+          backgroundImage: NetworkImage(
+              'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3463668003,3398677327&fm=58'),
+          child: new Text("李二"),
+          radius: 50, //半径
+        ),
+        Divider(
+          //这个高是容器的高度，设置不了线的高度
+          height: 10,
+          indent: 30, //距离左侧边距
+          color: Colors.red,
+        ),
+        AlertDialog(
+          title: Text('11111111'),
+          content: Text('222222222222222222222222'),
+        ),
+      ],
+    );
+  }
+
+  _bottomDialog(context) {
+    return showModalBottomSheet(
+        context: context,
+        builder: (context) => Container(
+              height: 140,
+              child: Column(
+                children: <Widget>[
+                  Text('我是bottom弹出框'),
+                  Image.asset(
+                    'images/aaa.png',
+                    width: 100,
+                    height: 100,
+                  ),
+                ],
+              ),
+            ));
   }
 }
